9a4e54e355ebb2de913aa5a42ca1a4be10064a67
wn83 <warren.j.zhou@gmail.com>
2019-03-03 12:09:33 -0800

    first commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector2.up * Time.deltaTime * speed, Space.World);
+
+		if (transform.position.y > 5) {
+			Destroy(gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.gameObject.tag == "boss") {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float reloadSpeed;
+	public float speed;
+	public float bulletSpawnVerticalOffset;
+
+	public int health;
+	int healthInt;
+	float healthPercent;
+	public int hitCooldown;
+	int hitCooldownCount;
+	
+	public GameObject PlayerHealth;
+	public GameObject bullet;
+
+	public Animator anim;
+
+
+	Image image;
+
+	float reloadCount;
+
+	void Start () {
+		reloadCount = reloadSpeed;
+		PlayerHealth = GameObject.Find("PlayerHealth");
+		image = PlayerHealth.GetComponent<Image>();
+		healthInt = health;
+		hitCooldownCount = 0;
+		anim.enabled = false;
+	}
+
+	void Update () {
+
+		healthPercent = (float) healthInt / health;
+		image.fillAmount = healthPercent;
+
+		reloadCount -= 0.1f;
+
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
+
+		transform.position = new Vector2(transform.position.x + (moveX * speed * Time.deltaTime), transform.position.y + (moveY * speed * Time.deltaTime));
+		if (Mathf.Abs(transform.position.x) > 6.93f) {
+			transform.position = new Vector3(Mathf.Sign(transform.position.x) * 6.93f, transform.position.y, 0);
+		}
+
+		if (Mathf.Abs(transform.position.y) > 4.95f) {
+			transform.position = new Vector3(transform.position.x, Mathf.Sign(transform.position.y) * 4.95f, 0);
+		}
+
+		if (reloadCount <= 0) {
+			Instantiate(bullet, new Vector2(transform.position.x, transform.position.y + bulletSpawnVerticalOffset), transform.rotation);
+			reloadCount = reloadSpeed;
+		}
+
+		hitCooldownCount -= 1;
+
+		if (hitCooldownCount > 0) {
+			anim.enabled = true;
+		} else {
+			anim.enabled = false;
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.gameObject.layer == 8 && hitCooldownCount <= 0) {
+			healthInt -= 5;
+			hitCooldownCount = hitCooldown;
+		}
+	}
+
+}
